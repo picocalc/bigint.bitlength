0367635a3e1897a60a63ef7643f12dae3ecc725f
Roman <dev@rman.dev>
2026-06-19 22:01:37 +0100

    fix linter warnings

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,8 +2,8 @@ import "bigint.bitlength";
 import assert from "node:assert";
 import { describe, it } from "node:test";
 
-describe("BigInt.bitLength", () => {
-  it("Correctly handles positive BigInt numbers", () => {
+void describe("BigInt.bitLength", () => {
+  void it("Correctly handles positive BigInt numbers", () => {
     assert.strictEqual(BigInt.bitLength(3n), 2n);
     assert.strictEqual(BigInt.bitLength(4n), 3n);
     assert.strictEqual(BigInt.bitLength(5n), 3n);
