973c706d9e770e3a04fe38e3d9f62c9f724951fc
Roman <dev@rman.dev>
2026-06-30 15:47:40 +0100

    chore: improve performance

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,49 @@ declare global {
 }
 
 const bitLength: typeof BigInt.bitLength = (n) => {
-  return BigInt(n.toString(2).length);
+  if (n === 0n) return 0n;
+  let absN = n < 0n ? -n : n;
+
+  let length = 1n;
+
+  // Exponentially scaled powers of 2
+  const shifts = [
+    16777216n,
+    8388608n,
+    4194304n,
+    2097152n,
+    1048576n,
+    524288n,
+    262144n,
+    131072n,
+    65536n,
+    32768n,
+    16384n,
+    8192n,
+    4096n,
+    2048n,
+    1024n,
+    512n,
+    256n,
+    128n,
+    64n,
+    32n,
+    16n,
+    8n,
+    4n,
+    2n,
+    1n,
+  ];
+
+  for (const shift of shifts) {
+    const test = absN >> shift;
+    if (test > 0n) {
+      absN = test;
+      length += shift;
+    }
+  }
+
+  return length;
 };
 
 // oxlint-disable-next-line no-unnecessary-condition => intended
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,8 +4,22 @@ import { describe, it } from "node:test";
 
 void describe("BigInt.bitLength", () => {
   void it("Correctly handles positive BigInt numbers", () => {
+    assert.strictEqual(BigInt.bitLength(1n), 1n);
+
+    assert.strictEqual(BigInt.bitLength(2n), 2n);
     assert.strictEqual(BigInt.bitLength(3n), 2n);
+
     assert.strictEqual(BigInt.bitLength(4n), 3n);
     assert.strictEqual(BigInt.bitLength(5n), 3n);
+    assert.strictEqual(BigInt.bitLength(6n), 3n);
+    assert.strictEqual(BigInt.bitLength(7n), 3n);
+  });
+
+  void it("Correctly handles big positive BigInt numbers", () => {
+    const bigNumberExp = 999_999n;
+    const bigNumber = 2n ** bigNumberExp;
+    assert.strictEqual(BigInt.bitLength(bigNumber - 1n), bigNumberExp);
+    assert.strictEqual(BigInt.bitLength(bigNumber), bigNumberExp + 1n);
+    assert.strictEqual(BigInt.bitLength(bigNumber + 1n), bigNumberExp + 1n);
   });
 });
